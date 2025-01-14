f2f1a132b3f06f09d99e648bcdea8d16ef0c2078
владимир назаров <volottnazarov@mail.ru>
2025-01-14 09:24:10 +0300

    Домашка 1

---
+++ HomeWork_1/Task_1/index.js
@@ -0,0 +1,22 @@
+﻿const musicCollection = {
+    albums: [
+        {title: "Human", artist: "Death", year: "1991"},
+        {title: "Battles In The North", artist: "Immortal", year: "1995"},
+        {title: "The Miracle", artist: "Queen", year: "1989"},
+        {title: "The Silent Enigma", artist: "Anathema", year: "1995"}
+    ],
+    [Symbol.iterator] () {
+        this.current = 0;
+        return this;
+    },
+    next() {
+        return this.current < this.albums.length
+            ? {done: false, value: this.albums[this.current ++] }
+            : {done: true};
+    }
+
+}
+
+for (const elem of musicCollection) {
+    console.log(elem.title ,"-", elem.artist, "(", elem.year, ")");
+}
---
+++ HomeWork_1/Task_2/index.js
@@ -0,0 +1,39 @@
+﻿const cookSpec = new Map(); //Специализация повара
+const dishCook = new Map(); //Блюда - повара
+const order = new Map();
+
+cookSpec.set("Виктор", "пицца")
+        .set("Ольга", "суши")
+        .set("Дмитрий", "дессерты");
+
+dishCook.set("Пицца 'Маргарита'", "Виктор")
+        .set("Пицца 'Пепперони'", "Виктор")
+        .set("Суши 'Филадельфия'", "Ольга")
+        .set("Суши 'Калифорния'", "Ольга")
+        .set("Тирамису", "Дмитрий")
+        .set("Чизкейк", "Дмитрий");
+
+let client_1 = {name: "Алексей"};
+let client_2 = {name: "Мария"};
+let client_3 = {name: "Ирина"};
+
+let order_1 = new Set();
+let order_2 = new Set();
+let order_3 = new Set();
+
+order_1.add("Пицца 'Маргарита")
+        .add("Чизкейк");
+
+order_2.add("Суши 'Филадельфия'")
+        .add("Тирамису");
+
+order_3.add("Пицца 'Пепперони'")
+        .add("Чизкейк");
+
+order.set(client_1, order_1)
+        .set(client_2, order_2)
+        .set(client_3, order_3);
+
+console.log(`Клиент ${client_1.name} заказал(а): ${[...order.get(client_1)]}`);
+console.log(`Клиент ${client_2.name} заказал(а): ${[...order.get(client_2)]}`);
+console.log(`Клиент ${client_3.name} заказал(а): ${[...order.get(client_3)]}`);
